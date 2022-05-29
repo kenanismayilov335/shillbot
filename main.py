3d5b626c98a3dbd2b808b5687cfd85d59e091c2f
Kazimbasha <97396649+Kazimbasha@users.noreply.github.com>
2022-05-30 00:40:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import datetime
 
 starttime = time.time()
 
-#H3T MAN
-api_id = 13886510
-api_hash = 'bb0ff72809482dc9a696f3856e90c9a9'
-message_here = ''''''
+#rogi roe
+api_id = 11362963
+api_hash = '02bb1a6fdeea5b719818db93156a89ad'
+message_here = open("shilllpost.txt")
+kazim = message_here.readline()
 
-
-groups = ['@defiandmore', '@uniswapgem123', '@KCC_Lovers', '@EliteCryptoShillers', '@RoseEnglishSignal', '@bscmoonz', '@uniswaptrollbox', '@MoonUrBSCtoken', '@UniswapGemGroup', '@bscmoon', '@UniswapObserver', '@DeFiPresales', '@chatbsc', '@cryptolux_tl', '@memeszn', '@ShillUSA', '@lauragem', '@bsc_moonshots', '@Defi_shill', '@GoodFellas_Cryptopicks', '@apeslounge', '@BscGemGlobal',]
+groups = ['@crypt0news2021', '@cryptolinksgroup', '@cryptolinks2020', '@cryptooo10', '@cryptoworld2024', '@exchangecity2020', '@tradersgr0up', '@bull2020bear', '@Trustedpr0ject', '@informationtoken', '@stockmarket2024', '@databl0ckchain', '@investments2022', '@bl0ckchainnews', '@communityaltcoin', '@poocoinshill', '@goshitcoins', '@CRYPTOMOONHOT', '@BSCBombers', '@BSCGemsENG', '@CryptoFamilyGroup', '@CryptoM00NShots', '@dexgemschat', '@to2themoonn', '@uniswaplegit', '@UniswapInvestorssss', '@uniswaprektplebs', '@whalers_club', '@xxxxxx1000000', '@whaler_club', '@uniswap_talk', '@bscgems247', '@CoinhuntCC', '@uniswapw', '@jumpsquad', '@uniswapall', '@uniswapbombsgroup', '@defiandmore', '@uniswapgem123', '@KCC_Lovers', '@EliteCryptoShillers', '@RoseEnglishSignal', '@bscmoonz', '@uniswaptrollbox', '@MoonUrBSCtoken', '@UniswapGemGroup', '@bscmoon', '@UniswapObserver', '@DeFiPresales', '@chatbsc', '@cryptolux_tl', '@memeszn', '@ShillUSA', '@lauragem', '@bsc_moonshots', '@Defi_shill', '@GoodFellas_Cryptopicks', '@apeslounge', '@BscGemGlobal',]
 
 failcount = 0;
  
@@ -19,7 +19,7 @@ while True:
     with TelegramClient('anon', api_id, api_hash) as client:
         for x in groups:
             try:
-                client.loop.run_until_complete(client.send_message (x, message_here))
+                client.loop.run_until_complete(client.send_message (x, kazim))
             
             
             
@@ -27,5 +27,5 @@ while True:
                 print(x, sys.exc_info()[0])
                 failcount += 1
     print(datetime.datetime.now(), str(failcount/len(groups) * 100) + '%')
-    time.sleep(30 - ((time.time() - starttime) % 1))
+    time.sleep(120 - ((time.time() - starttime) % 1))
 
